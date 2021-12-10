28acc2a75dee66a086786b0e4aedd13acad7d475
Daniel Gallichan <dgallichan@yahoo.com>
2021-12-10 10:38:34 +0000

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,19 @@
 input.onGesture(Gesture.Shake, function () {
-    basic.showString("Merry Xmas!")
+    flashing = false
+    ScrolText.showString(
+    "Merry Xmas!",
+    SCROLL_DIR.LEFT,
+    SCROLL_ROTATE.SR_0,
+    100
+    )
+    basic.pause(3000)
+    flashing = true
 })
+let flashing = false
+flashing = true
 basic.forever(function () {
-    led.toggle(randint(0, 4), randint(0, 4))
+    if (flashing == true) {
+        led.toggle(randint(0, 4), randint(0, 4))
+    }
     basic.pause(50)
 })
